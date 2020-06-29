dae52353044892b4a7c725a11b1ac1bbe9a1a89d
Jack Shephard <jack.shephard@symphonyretailai.com>
2020-06-29 16:40:46 +0100

    more fluff

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 from flask import Flask
-myapp = Flask(__name__)
+from datetime import datetime
+import re
+
+app = Flask(__name__)
 
 
-@myapp.route("/")
-def hello():
-    return "Hello Flask, on Azure App Service for Linux"
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return content
